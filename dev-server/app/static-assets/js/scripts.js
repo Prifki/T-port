ee5f5a1743397d1ded07bf8473ff66b4f9f24a64
Prifki <yar95b@ya.ru>
2018-10-17 22:59:34 +0300

    new header

--- dev-server/app/static-assets/js/scripts.js
+++ dev-server/app/static-assets/js/scripts.js
@@ -1,3 +1,15 @@
 function burgerMenuToggle() {
-	document.getElementsByClassName("burger-base")[0].classList.toggle("burger-close");
+	document.getElementsByClassName("floating-menu")[0].classList.toggle("show-menu");
+	if(document.getElementsByClassName("mobile-find-field")[0].classList.toggle("show-mobile-search")){
+		document.getElementsByClassName("mobile-find-field")[0].classList.toggle("show-mobile-search");
+	}
+}
+
+function mobileSearch() {
+	document.getElementsByClassName("mobile-find-field")[0].classList.toggle("show-mobile-search");
+	if(document.getElementsByClassName("floating-menu")[0].classList.toggle("show-menu")){
+		document.getElementsByClassName("floating-menu")[0].classList.toggle("show-menu");
+	}
+	if(document.getElementsByClassName("ham")[0].classList.toggle("active"))
+		document.getElementsByClassName("ham")[0].classList.toggle("active");
 }
