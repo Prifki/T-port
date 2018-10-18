2a4bc40714954e75d427c8ce96ddf9cb2da6a471
Prifki <yar95b@ya.ru>
2018-10-18 04:34:24 +0300

    index page menus are ready

--- dev-server/app/static-assets/js/scripts.js
+++ dev-server/app/static-assets/js/scripts.js
@@ -1,15 +1,37 @@
 function burgerMenuToggle() {
-	document.getElementsByClassName("floating-menu")[0].classList.toggle("show-menu");
-	if(document.getElementsByClassName("mobile-find-field")[0].classList.toggle("show-mobile-search")){
-		document.getElementsByClassName("mobile-find-field")[0].classList.toggle("show-mobile-search");
-	}
+	var mobileFindFielf = document.getElementsByClassName("mobile-find-field")[0],
+	floatingMenu = document.getElementsByClassName("floating-menu")[0];
+	floatingMenu.classList.toggle("show-menu");
+	toggleClass(mobileFindFielf,"show-mobile-search")
 }
 
 function mobileSearch() {
-	document.getElementsByClassName("mobile-find-field")[0].classList.toggle("show-mobile-search");
-	if(document.getElementsByClassName("floating-menu")[0].classList.toggle("show-menu")){
-		document.getElementsByClassName("floating-menu")[0].classList.toggle("show-menu");
-	}
-	if(document.getElementsByClassName("ham")[0].classList.toggle("active"))
-		document.getElementsByClassName("ham")[0].classList.toggle("active");
+	var mobileFindFielf = document.getElementsByClassName("mobile-find-field")[0],
+	floatingMenu = document.getElementsByClassName("floating-menu")[0],
+	ham = document.getElementsByClassName("ham")[0];
+
+	mobileFindFielf.classList.toggle("show-mobile-search");
+	toggleClass(floatingMenu,"show-menu")
+	toggleClass(ham,"active")
+}
+
+function showFavorites(){
+	var loginMenu = document.getElementsByClassName("login-menu")[0];
+	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
+
+	toggleClass(loginMenu,"show-additional-menu")
+	favoritesMenu.classList.toggle("show-additional-menu");
+}
+
+function showLoginMenu(){
+	var loginMenu = document.getElementsByClassName("login-menu")[0],
+	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
+
+	loginMenu.classList.toggle("show-additional-menu");
+	toggleClass(favoritesMenu,"show-additional-menu")
+}
+
+function toggleClass(obj,cla){
+	if(obj.classList.toggle(cla))
+		obj.classList.toggle(cla)
 }
