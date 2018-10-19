06a90d649f0e26098f05eb352224f906c110d514
Prifki <yar95b@ya.ru>
2018-10-19 06:05:48 +0300

    dont actially remember what Ive done, gonna sleep now

--- dev-server/app/static-assets/js/scripts.js
+++ dev-server/app/static-assets/js/scripts.js
@@ -1,28 +1,22 @@
-function burgerMenuToggle() {
-	var mobileFindFielf = document.getElementsByClassName("mobile-find-field")[0],
+function burgerMenuToggle(){
+	var mobileFindField = document.getElementsByClassName("mobile-find-field")[0],
 	floatingMenu = document.getElementsByClassName("floating-menu")[0];
 	floatingMenu.classList.toggle("show-menu");
-	ifOnThenOff(mobileFindFielf,"show-mobile-search");
+	ifOnThenOff(mobileFindField,"show-mobile-search");
 }
 
-function showCard(){
-	var transportCard = document.getElementsByClassName("card")[0];
-	transportCard.classList.toggle("show-card");
-	transportCard.scrollIntoView({block: "start", behavior: "smooth"});
-}
-
-function mobileSearch() {
-	var mobileFindFielf = document.getElementsByClassName("mobile-find-field")[0],
+function mobileSearch(){
+	var mobileFindField = document.getElementsByClassName("mobile-find-field")[0],
 	floatingMenu = document.getElementsByClassName("floating-menu")[0],
 	ham = document.getElementsByClassName("ham")[0];
 
-	mobileFindFielf.classList.toggle("show-mobile-search");
+	mobileFindField.classList.toggle("show-mobile-search");
 	ifOnThenOff(floatingMenu,"show-menu");
 	ifOnThenOff(ham,"active");
 }
 
 function showFavorites(){
-	var loginMenu = document.getElementsByClassName("login-menu")[0];
+	var loginMenu = document.getElementsByClassName("login-menu")[0],
 	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
 
 	ifOnThenOff(loginMenu,"show-additional-menu");
@@ -40,4 +34,30 @@ function showLoginMenu(){
 function ifOnThenOff(obj,cla){
 	if(obj.classList.toggle(cla))
 		obj.classList.toggle(cla);
+}
+
+function closeAll(){
+	mobileFindField = document.getElementsByClassName("mobile-find-field")[0],
+	floatingMenu = document.getElementsByClassName("floating-menu")[0],
+	ham = document.getElementsByClassName("ham")[0],
+	loginMenu = document.getElementsByClassName("login-menu")[0],
+	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
+
+	menusToClose=[mobileFindField,floatingMenu,ham,loginMenu,favoritesMenu];
+	classToClose = ["show-mobile-search","show-menu","active","show-additional-menu","show-additional-menu"];
+
+	for(i=0;i<menusToClose.length;i++){
+		ifOnThenOff(menusToClose[i],classToClose[i])
+	}
+}
+
+function showCard(){
+	var transportCard = document.getElementsByClassName("card")[0];
+	transportCard.classList.toggle("show-card");
+	transportCard.scrollIntoView({block: "start", behavior: "smooth"});
+}
+
+function closeFindMenu(){
+	var findMenu = document.getElementsByClassName("find-a-route-menu")[0];
+	findMenu.classList.toggle("find-a-route-wrapped");
 }
