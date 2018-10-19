2f361987023b059f550746a6966ba36ad3f2b7d4
Prifki <yar95b@ya.ru>
2018-10-19 04:20:50 +0300

    opening a transport card added

--- dev-server/app/static-assets/js/scripts.js
+++ dev-server/app/static-assets/js/scripts.js
@@ -2,7 +2,13 @@ function burgerMenuToggle() {
 	var mobileFindFielf = document.getElementsByClassName("mobile-find-field")[0],
 	floatingMenu = document.getElementsByClassName("floating-menu")[0];
 	floatingMenu.classList.toggle("show-menu");
-	toggleClass(mobileFindFielf,"show-mobile-search")
+	ifOnThenOff(mobileFindFielf,"show-mobile-search");
+}
+
+function showTransportCard(){
+	var transportCard = document.getElementsByClassName("transport-card")[0];
+	transportCard.classList.toggle("show-card");
+	transportCard.scrollIntoView({block: "start", behavior: "smooth"});
 }
 
 function mobileSearch() {
@@ -11,16 +17,16 @@ function mobileSearch() {
 	ham = document.getElementsByClassName("ham")[0];
 
 	mobileFindFielf.classList.toggle("show-mobile-search");
-	toggleClass(floatingMenu,"show-menu")
-	toggleClass(ham,"active")
+	ifOnThenOff(floatingMenu,"show-menu");
+	ifOnThenOff(ham,"active");
 }
 
 function showFavorites(){
 	var loginMenu = document.getElementsByClassName("login-menu")[0];
 	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
 
-	toggleClass(loginMenu,"show-additional-menu")
-	favoritesMenu.classList.toggle("show-additional-menu");
+	ifOnThenOff(loginMenu,"show-additional-menu");
+	favoritesMenu.classList.toggle("show-additional-menu");;
 }
 
 function showLoginMenu(){
@@ -28,10 +34,10 @@ function showLoginMenu(){
 	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
 
 	loginMenu.classList.toggle("show-additional-menu");
-	toggleClass(favoritesMenu,"show-additional-menu")
+	ifOnThenOff(favoritesMenu,"show-additional-menu");
 }
 
-function toggleClass(obj,cla){
+function ifOnThenOff(obj,cla){
 	if(obj.classList.toggle(cla))
-		obj.classList.toggle(cla)
+		obj.classList.toggle(cla);
 }
