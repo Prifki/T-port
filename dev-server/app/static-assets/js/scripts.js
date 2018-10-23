81a8b9f88e893a51b1c32ac094211b5b8cc4e9bb
Prifki <yar95b@ya.ru>
2018-10-23 04:22:57 +0300

    editor's functionality has been added

--- dev-server/app/static-assets/js/scripts.js
+++ dev-server/app/static-assets/js/scripts.js
@@ -3,14 +3,12 @@ function burgerMenuToggle(){
 	floatingMenu = document.getElementsByClassName("floating-menu")[0],
 	ham = document.getElementsByClassName("ham")[0],
 	loginMenu = document.getElementsByClassName("login-menu")[0],
-	favoritesMenu = document.getElementsByClassName("favorites-menu")[0],
-	findMenu = document.getElementsByClassName("find-a-route-menu")[0];
+	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
 	
 	floatingMenu.classList.toggle("show-menu");
 	ifOnThenOff(loginMenu,"show-additional-menu");
 	ifOnThenOff(favoritesMenu,"show-additional-menu");
 	ifOnThenOff(mobileFindField,"show-mobile-search");
-	ifOffThenOn(findMenu,"find-a-route-wrapped");
 }
 
 function mobileSearch(){
@@ -18,15 +16,13 @@ function mobileSearch(){
 	floatingMenu = document.getElementsByClassName("floating-menu")[0],
 	ham = document.getElementsByClassName("ham")[0],
 	loginMenu = document.getElementsByClassName("login-menu")[0],
-	favoritesMenu = document.getElementsByClassName("favorites-menu")[0],
-	findMenu = document.getElementsByClassName("find-a-route-menu")[0];
+	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
 
 	mobileFindField.classList.toggle("show-mobile-search");
 	ifOnThenOff(floatingMenu,"show-menu");
 	ifOnThenOff(loginMenu,"show-additional-menu");
 	ifOnThenOff(favoritesMenu,"show-additional-menu");
 	ifOnThenOff(ham,"active");
-	ifOffThenOn(findMenu,"find-a-route-wrapped");
 }
 
 function showFavorites(){
@@ -34,39 +30,28 @@ function showFavorites(){
 	floatingMenu = document.getElementsByClassName("floating-menu")[0],
 	ham = document.getElementsByClassName("ham")[0],
 	loginMenu = document.getElementsByClassName("login-menu")[0],
-	favoritesMenu = document.getElementsByClassName("favorites-menu")[0],
-	findMenu = document.getElementsByClassName("find-a-route-menu")[0];
+	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
 
 	favoritesMenu.classList.toggle("show-additional-menu");
 	ifOnThenOff(loginMenu,"show-additional-menu");
 	ifOnThenOff(floatingMenu,"show-menu");
 	ifOnThenOff(ham,"active");
 	ifOnThenOff(mobileFindField,"show-mobile-search");
-	if (window.innerWidth <= 470)
-		ifOffThenOn(findMenu,"find-a-route-wrapped");
 }
 
-function editFavorites(){
-	deleteButtons = document.getElementsByClassName("delete-button");
-	for (i=0; i < deleteButtons.length; i++)
-		deleteButtons[i].style.display = 'block';
-}
 
 function showLoginMenu(){
 	mobileFindField = document.getElementsByClassName("mobile-find-field")[0],
 	floatingMenu = document.getElementsByClassName("floating-menu")[0],
 	ham = document.getElementsByClassName("ham")[0],
 	loginMenu = document.getElementsByClassName("login-menu")[0],
-	favoritesMenu = document.getElementsByClassName("favorites-menu")[0],
-	findMenu = document.getElementsByClassName("find-a-route-menu")[0];
+	favoritesMenu = document.getElementsByClassName("favorites-menu")[0];
 
 	loginMenu.classList.toggle("show-additional-menu");
 	ifOnThenOff(favoritesMenu,"show-additional-menu");
 	ifOnThenOff(floatingMenu,"show-menu");
 	ifOnThenOff(ham,"active");
 	ifOnThenOff(mobileFindField,"show-mobile-search");
-	if (window.innerWidth <= 470)
-		ifOffThenOn(findMenu,"find-a-route-wrapped");
 }
 
 function ifOnThenOff(obj,cla){
@@ -90,6 +75,95 @@ function showCard(){
 	transportCard.scrollIntoView({block: "start", behavior: "smooth"});
 }
 
+function editFavorites(){
+	deleteButtons = document.getElementsByClassName("delete-button");
+	if(document.getElementsByClassName('edit-button')[0].firstChild.textContent=='more_vert'){
+		for (i=0; i < deleteButtons.length; i++)
+			deleteButtons[i].style.display = 'block';
+		document.getElementsByClassName('edit-button')[0].firstChild.textContent='more_horiz';
+	}
+	else {
+		for (i=0; i < deleteButtons.length; i++)
+			deleteButtons[i].style.display = 'none';
+		document.getElementsByClassName('edit-button')[0].firstChild.textContent='more_vert';
+	}
+}
+
+function removeFromFavorites(button){
+	button.parentElement.remove('');
+}
+
+function editTable(){
+	try{
+		const tbody = document.getElementsByTagName('tbody')[0];
+
+		if (tbody.firstChild.lastChild.innerText != "Remove"){
+			const Rows = Array.from(tbody.rows);
+			Rows.splice(0,1);
+
+			addEditColumn(tbody);
+
+			for (row in Rows){
+				const i = document.createElement('i'),
+				td = document.createElement('td');
+				td.appendChild(i);
+				i.setAttribute('class','material-icons table-edit-button');
+				i.innerText='edit';
+				Rows[row].appendChild(td);
+			}
+			
+			addRemoveColumn(tbody);
+
+			for (row in Rows){
+				const i = document.createElement('i'),
+				td = document.createElement('td');
+				td.appendChild(i);
+				i.setAttribute('class','fa fa-minus table-edit-button');
+				Rows[row].appendChild(td);
+			}
+
+			for (let i=0; i<tbody.firstChild.children.length-2; i++){
+				const td = document.createElement('td'),
+				editInput = document.createElement('input');
+				editInput.setAttribute('class', 'table-edit-input');
+				editInput.setAttribute('type', 'text');
+				td.appendChild(editInput);
+				tbody.lastChild.appendChild(td);
+			}
+			addPlusButton(tbody);
+			const td = document.createElement('td');
+			tbody.lastChild.appendChild(td);
+		}
+	}
+	catch (error) {
+		console.log("An error has been caught: " + error);
+	}
+
+	function addPlusButton(tbody){
+		const td = document.createElement('td'),
+		i = document.createElement('i');
+		i.setAttribute('class','material-icons table-edit-button');
+		i.innerText='add_circle_outline';
+		td.appendChild(i);
+		tbody.lastChild.appendChild(td);
+	}
+
+	function addEditColumn(tbody){
+		tr = document.createElement('tr'),
+		th = document.createElement('th');
+		th.innerText="Edit";
+		tbody.firstChild.appendChild(th);
+		tbody.appendChild(tr);
+	}
+
+	function addRemoveColumn(tbody){
+		tr = document.createElement('tr'),
+		th = document.createElement('th');
+		th.innerText="Remove";
+		tbody.firstChild.appendChild(th);
+		tbody.appendChild(tr);
+	}
+}
 
 
 /*function closeAll(){
