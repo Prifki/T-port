37b47a3097d739f82b13992d5000fc9a6787c12d
Prifki <yar95b@ya.ru>
2018-11-03 04:49:50 +0300

    Going on splitting into components

--- t-port/src/components/Main.js
+++ t-port/src/components/Main.js
@@ -5,11 +5,13 @@ import Pagination from './page_components/Pagination';
 import Card from './page_components/Card';
 import LoginMenu from './menus/LoginMenu';
 import FavoritesMenu from './menus/FavoritesMenu';
+import BurgerNavBar from './menus/BurgerNavBar';
 
 class Main extends Component {
   render() {
     return (
         <main>
+            <BurgerNavBar />
             <div className="substrate">
                 <h2 className="page-name">Transport</h2>
                 <FilterByType />
@@ -17,8 +19,8 @@ class Main extends Component {
                 <Pagination />
                 <Card />
             </div>
-            {/*<LoginMenu />*/}
-            <FavoritesMenu />
+            {/*<LoginMenu />
+            <FavoritesMenu />*/}
         </main>
     );
   }
