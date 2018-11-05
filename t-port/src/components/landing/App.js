49c15140eabdc82c0f7c3cca46468038a85113e1
arifbalaev <Index241195@gmail.com>
2018-11-05 07:28:53 +0300

    react router implemented

--- t-port/src/components/landing/App.js
+++ t-port/src/components/landing/App.js
@@ -1,26 +1,29 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import {Route, Switch} from "react-router-dom";
 import Header from './header/Header';
 import Footer from './footer/Footer';
-import RoutesContainer from '../pages/RoutesContainer';
-import TransportsContainer from '../pages/TransportsContainer';
 import StopsContainer from '../pages/StopsContainer';
 import FindRouteContainer from '../pages/FindRouteContainer';
+import RoutesContainer from '../pages/RoutesContainer';
+import TransportsContainer from '../pages/TransportsContainer';
 
 class App extends Component {
-  render() {
-    return (
-      <>
-        <Header />
-        <StopsContainer />
-        {/*<FindRouteContainer />
-        <TransportsContainer />
-        <RoutesContainer />
-        <StopsContainer />
-        */}
-        <Footer />
-      </>
-    );
-  }
+    render() {
+        return (
+            <>
+                <Header/>
+
+                <Switch>
+                    <Route exact path='/' component={FindRouteContainer}/>
+                    <Route path='/transports' component={TransportsContainer}/>
+                    <Route path='/routes' component={RoutesContainer}/>
+                    <Route path='/stops' component={StopsContainer}/>
+                </Switch>
+
+                <Footer/>
+            </>
+        );
+    }
 }
 
 export default App;
