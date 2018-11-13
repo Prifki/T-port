54d328e9b517383f671e4085c83b25e422a6edd8
Prifki <yar95b@ya.ru>
2018-11-13 11:02:43 +0300

    making modal card a stupid element

--- t-port/src/components/pages/presentational/CardModal.js
+++ t-port/src/components/pages/presentational/CardModal.js
@@ -1,31 +1,24 @@
-import React, { Component } from 'react';
+import React from 'react';
 import Table from './Table';
 import BookmarkButton from './BookmarkButton';
 import CloseButton from './CloseButton';
 import GoogleMap from './GoogleMap';
 
-class CardModal extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-    }
-  }
-  render() {
+function CardModal(props) {
     return (
       <>
         <div className="blur"></div>
         <div className="card card--modal">
-            <BookmarkButton onClick={() => this.props.addToFavorites(this.props.title)}/>
-            <CloseButton onClick={this.props.closeModalCard} />
-            <h3 className="card__title">{this.props.title}</h3>
-            <Table header = {this.props.header} rows = {this.props.rows} isAdmin={this.props.isAdmin} addItem={this.props.addItem} isEditingMode={this.props.isEditingMode} toggleEditingMode={this.props.toggleEditingMode}/>
-            {this.props.isMapNeededOnModalCard ? 
-            <div className="google-map--small"><GoogleMap markers={this.props.modalCardMarkers} polyline={this.props.polyline} /></div>
+            <BookmarkButton onClick={() => props.addToFavorites(props.title)}/>
+            <CloseButton onClick={props.closeModalCard} />
+            <h3 className="card__title">{props.title}</h3>
+            <Table header = {props.header} rows = {props.rows} isAdmin={props.isAdmin} addItem={props.addItem} isEditingMode={props.isEditingMode} toggleEditingMode={props.toggleEditingMode}/>
+            {props.isMapNeededOnModalCard ? 
+            <div className="google-map--small"><GoogleMap markers={props.modalCardMarkers} polyline={props.polyline} /></div>
             : null}
         </div>
       </>
     );
-  }
 }
 
 export default CardModal;
